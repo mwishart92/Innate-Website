932c03a9771feb710759d963990a4f87852df1a5
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2025-05-30 09:52:45 +0500

    dynamic the work page and added new 4 slides

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -7,6 +7,7 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 // Import WorkSlider components
+import WorkSlider from "@/components/Work/WorkSlider";
 import WorkSlider0 from "@/components/Work/WorkSlider0";
 import WorkSlider1 from "@/components/Work/WorkSlider1";
 import WorkSlider2 from "@/components/Work/WorkSlider2";
@@ -27,22 +28,312 @@ gsap.registerPlugin(ScrollTrigger);
 import "./Work.css";
 
 // Define an array of WorkSlider components
+const slidesData: {
+  title: string;
+  location: string;
+  url?: string;
+  media?: { src: string; type: "image" | "video" }[];
+}[] = [
+  {
+    title: "North Seattle Kitchen",
+    location: "North Seattle",
+    url: "/project/north-seattle-kitchen",
+    media: [
+      { src: "/images/project/seattle-kitchen/IMG_1568.webp", type: "image" },
+      { src: "/images/project/seattle-kitchen/IMG_1567.webp", type: "image" },
+      { src: "/images/project/seattle-kitchen/IMG_1570.webp", type: "image" },
+      { src: "/images/project/seattle-kitchen/IMG_1572.webp", type: "image" },
+    ],
+  },
+  {
+    title: "Shoreline Residence",
+    location: "Shoreline, Washington",
+    url: "/project/shoreline-residence",
+    media: [
+      {
+        src: "/images/project/shoreline-residence/COVER-D5_a7 2_20240625_005152.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/shoreline-residence/D5_Scene 21 1_20240628_105629.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/shoreline-residence/D5_Scene 22 2_20240628_104252.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/shoreline-residence/D5_Scene 23 1_20240628_085609.webp",
+        type: "image",
+      },
+    ],
+  },
+  {
+    title: "Freitas Bathroom",
+    location: "Capitol Hill – Seattle, Washington",
+    url: "/project/freitas-bathroom",
+    media: [
+      {
+        src: "/images/project/freitas-bathroom/6L1A8442-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/freitas-bathroom/6L1A8451-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/freitas-bathroom/6L1A8447-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/freitas-bathroom/6L1A8433-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/freitas-bathroom/6L1A8426-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/freitas-bathroom/6L1A8431-Enhanced-NR.webp",
+        type: "image",
+      },
+    ],
+  },
+  {
+    title: "Knight Residence",
+    location: "Fremont – Seattle, Washington",
+    url: "/project/knight-residence",
+    media: [
+      {
+        src: "/images/project/knight-residence/05.25_Innate-NW_16x9 No SpeedRamp_No Sound.mp4",
+        type: "video",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8383-Enhanced-NR.webp",
+        type: "image",
+      },
+      { src: "/images/project/knight-residence/6L1A8374.webp", type: "image" },
+      {
+        src: "/images/project/knight-residence/6L1A8322-Enhanced-NR.webp",
+        type: "image",
+      },
+      { src: "/images/project/knight-residence/6L1A8371.webp", type: "image" },
+      {
+        src: "/images/project/knight-residence/6L1A8365-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8358-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8344-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8341-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8335-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8327-Enhanced-NR.webp",
+        type: "image",
+      },
+      { src: "/images/project/knight-residence/6L1A8324.webp", type: "image" },
+      {
+        src: "/images/project/knight-residence/6L1A8319-Enhanced-NR.webp",
+        type: "image",
+      },
+      {
+        src: "/images/project/knight-residence/6L1A8386-Enhanced-NR.webp",
+        type: "image",
+      },
+    ],
+  },
+  {
+    title: "Desert Oasis",
+    location: "Palm Springs, California",
+    url: "/project/desert-oasis",
+    media: [
+      { src: "/images/work/DS8.png", type: "image" },
+      { src: "/images/work/DS2.png", type: "image" },
+      { src: "/images/work/DS1.png", type: "image" },
+      { src: "/images/work/DS3.png", type: "image" },
+      { src: "/images/work/DS4.png", type: "image" },
+      { src: "/images/work/DS5.png", type: "image" },
+      { src: "/images/work/DS6.png", type: "image" },
+      { src: "/images/work/DS7.png", type: "image" },
+    ],
+  },
+  {
+    title: "Gray Bathroom",
+    location: "Seattle, Washington",
+    url: "/project/gray-bathroom",
+    media: [
+      { src: "/images/work/GB4.jpg", type: "image" },
+      { src: "/images/work/GB2.jpg", type: "image" },
+      { src: "/images/work/GB3.jpg", type: "image" },
+      { src: "/images/work/GB1.jpg", type: "image" },
+      { src: "/images/work/GB5.jpg", type: "image" },
+      { src: "/images/work/GB6.jpg", type: "image" },
+      { src: "/images/work/GB7.jpg", type: "image" },
+      { src: "/images/work/GB8.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Wheat Ridge ADU",
+    location: "Kirkland, Washington",
+    url: "/project/wheat-ridge-adu",
+    media: [
+      { src: "/images/work/KA1.jpg", type: "image" },
+      { src: "/images/work/KA2.jpg", type: "image" },
+      { src: "/images/work/KA3.jpg", type: "image" },
+      { src: "/images/work/KA4.jpg", type: "image" },
+      { src: "/images/work/KA5.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Kung Fu Tea x TKK",
+    location: "Renton, Washington",
+    url: "/project/kung-fu-tea",
+    media: [
+      { src: "/images/work/KFT5.png", type: "image" },
+      { src: "/images/work/KFT4.png", type: "image" },
+      { src: "/images/work/KFT3.png", type: "image" },
+      { src: "/images/work/KFT2.png", type: "image" },
+      { src: "/images/work/KFT1.png", type: "image" },
+    ],
+  },
+  {
+    title: "Madrona Remodel",
+    location: "Seattle, Washington",
+    url: "/project/madrona-remodel",
+    media: [
+      { src: "/images/work/MR1.png", type: "image" },
+      { src: "/images/work/MR2.png", type: "image" },
+      { src: "/images/work/MR3.png", type: "image" },
+      { src: "/images/work/MR4.png", type: "image" },
+      { src: "/images/work/MR5.png", type: "image" },
+      { src: "/images/work/MR6.png", type: "image" },
+    ],
+  },
+  {
+    title: "Modern ADU",
+    location: "Bellevue, Washington",
+    media: [
+      { src: "/images/work/MA1.jpg", type: "image" },
+      { src: "/images/work/MA2.jpg", type: "image" },
+      { src: "/images/work/MA3.jpg", type: "image" },
+      { src: "/images/work/MA4.jpg", type: "image" },
+      { src: "/images/work/MA5.jpg", type: "image" },
+      { src: "/images/work/MA6.jpg", type: "image" },
+      { src: "/images/work/MA7.jpg", type: "image" },
+      { src: "/images/work/MA8.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Solar Retreat",
+    location: "Rainier, Washington",
+    media: [
+      { src: "/images/work/solar2.jpg", type: "image" },
+      { src: "/images/work/solar1.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Wedgewood ADU",
+    location: "North Seattle, Washington",
+    media: [
+      {
+        src: "https://firebasestorage.googleapis.com/v0/b/innate-bb90b.firebasestorage.app/o/bosma-adu.mp4?alt=media&token=0c906c3e-ed25-4082-8230-f5941f54e9ce",
+        type: "video",
+      },
+    ],
+  },
+  {
+    title: "Japandi ADU",
+    location: "Magnolia, Washington",
+    media: [
+      { src: "/images/work/JA1.jpg", type: "image" },
+      { src: "/images/work/JA2.jpg", type: "image" },
+      { src: "/images/work/JA4.jpg", type: "image" },
+      { src: "/images/work/JA5.jpg", type: "image" },
+      { src: "/images/work/JA6.jpg", type: "image" },
+      { src: "/images/work/JA7.jpg", type: "image" },
+      { src: "/images/work/JA8.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Norman Bathroom",
+    location: "West Seattle, Washington",
+    media: [
+      { src: "/images/work/NB3.jpg", type: "image" },
+      { src: "/images/work/NB2.jpg", type: "image" },
+      { src: "/images/work/NB1.jpg", type: "image" },
+      { src: "/images/work/NB4.jpg", type: "image" },
+      {
+        src: "https://firebasestorage.googleapis.com/v0/b/innate-bb90b.firebasestorage.app/o/Norman-bath.mp4?alt=media&token=963a355e-149c-4735-87f2-911ad70f6445",
+        type: "video",
+      },
+    ],
+  },
+  {
+    title: "Pellegrino Remodel",
+    location: "Denver, Colorado",
+    media: [
+      {
+        src: "https://firebasestorage.googleapis.com/v0/b/innate-bb90b.firebasestorage.app/o/pellegrino-remodel.mp4?alt=media&token=d42fe844-9e50-4b63-8e43-3fcf398e1240",
+        type: "video",
+      },
+      { src: "/images/work/PR1.png", type: "image" },
+    ],
+  },
+  {
+    title: "Pierre ADU",
+    location: "Tacoma, Washington",
+    media: [
+      { src: "/images/work/PA2.jpg", type: "image" },
+      { src: "/images/work/PA1.jpg", type: "image" },
+    ],
+  },
+  {
+    title: "Victoria Bathroom",
+    location: "Bothell, Washington",
+    media: [
+      {
+        src: "https://firebasestorage.googleapis.com/v0/b/innate-bb90b.firebasestorage.app/o/victoria%20bathroom.mp4?alt=media&token=7c9e1c15-fffa-4c74-b6fc-50b0cee6a94b",
+        type: "video",
+      },
+    ],
+  },
+  {
+    title: "West Seattle Duplex",
+    location: "West Seattle, Washington",
+    media: [
+      { src: "/images/work/WSD2.jpg", type: "image" },
+      { src: "/images/work/WSD1.jpg", type: "image" },
+      { src: "/images/work/WSD3.jpg", type: "image" },
+    ],
+  },
+];
 const sliders: React.ElementType[] = [
-  WorkSlider1,
-  WorkSlider2,
-  WorkSlider4,
-  WorkSlider5,
-  WorkSlider6,
-  WorkSlider7,
-  WorkSlider12,
-  WorkSlider0,
-  WorkSlider3,
-  WorkSlider8,
-  WorkSlider9,
-  WorkSlider10,
-  WorkSlider11,
-  WorkSlider13,
-  WorkSlider14,
+  // WorkSlider1,
+  // WorkSlider2,
+  // WorkSlider4,
+  // WorkSlider5,
+  // WorkSlider6,
+  // WorkSlider7,
+  // WorkSlider12,
+  // WorkSlider0,
+  // WorkSlider3,
+  // WorkSlider8,
+  // WorkSlider9,
+  // WorkSlider10,
+  // WorkSlider11,
+  // WorkSlider13,
+  // WorkSlider14,
 ];
 
 const Work: React.FC = () => {
@@ -165,7 +456,7 @@ const Work: React.FC = () => {
             isDesktop ? "h-screen overflow-hidden" : ""
           }`}
         >
-          {sliders.map((SliderComponent, index) => (
+          {slidesData.map((data, index) => (
             <div
               key={index}
               className={`work-slider w-full min-h-screen flex items-center ${
@@ -173,7 +464,12 @@ const Work: React.FC = () => {
               } bg-white`}
             >
               <div className="h-full tab:h-[80%] mob:h-auto w-full rounded-t-[24px] rounded-b-[18px]">
-                <SliderComponent />
+                <WorkSlider
+                  title={data.title}
+                  location={data.location}
+                  url={data.url || ""}
+                  media={data.media}
+                />
               </div>
             </div>
           ))}
---
+++ src/components/Work/WorkSlider/index.tsx
@@ -0,0 +1,112 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { Keyboard, Navigation, Scrollbar, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/scrollbar";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Link from "next/link";
+import Text from "@/components/ui/Text";
+import arrowwhite from "@/public/images/work/Arrowwhite.png";
+import "../slider.css";
+import MediaSlide from "../MediaSlide";
+// import leftarrow from '@/public/images/work/Arrowleftblue.png'
+import arrow from "@/public/images/work/Arrow.png";
+
+function WorkSlider({
+  title,
+  location,
+  media,
+  url,
+}: {
+  title: string;
+  location: string;
+  url: string;
+  media: { src: string; type: "image" | "video" }[] | undefined;
+}) {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const swiper = useSwiper();
+
+  const handleprevbtn = () => {
+    swiper?.slidePrev();
+    console.log("slidePrev");
+  };
+
+  const handleNextvbtn = () => {
+    swiper?.slideNext();
+    console.log("slideNext");
+  };
+
+  return (
+    <div className="w-full h-screen overflow-hidden flex justify-center items-center relative">
+      <div className="absolute inset-0 flex justify-center items-center z-20">
+        <div className="text-center p-5 mob:p-14 w-full bg-opacity-50">
+          <div>
+            <Text
+              as="h2"
+              className="text-[74px] font-semibold text-[#FFFFFF] mob:text-[29.29px]"
+            >
+              {title}
+            </Text>
+            <Text className="text-[28px] font-normal  text-[#FFFFFF] mb-3 flex items-center gap-1 text-center justify-center mob:text-[11.84px]">
+              {location}
+            </Text>
+            {url && (
+              <Link
+                href={url}
+                className="mt-10 mob:mt-6 font-normal max-w-[200px] h-[62.69px] mx-auto flex items-center justify-center text-center gap-1 text-[18px] mob:text-[12px] text-white border border-[#FFFFFF] px-[3px] py-[17.34px] mob:max-w-[140px] mob:px-[7px] mob:py-[7.33px] mob:h-[40px] z-20 relative"
+              >
+                LEARN MORE
+                <Image src={arrowwhite} alt="" width={18} height={18} />
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+      <div className="w-full overflow-hidden flex justify-center items-center relative">
+        <Image
+          className="cards-custom-prev10 absolute left-4 top-1/2 transform -translate-y-1/2 cursor-pointer z-20 rotate-180"
+          onClick={handleprevbtn}
+          src={arrow}
+          alt="moveprevbtn"
+        />
+        <Image
+          className="cards-custom-next10 absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer z-20"
+          onClick={handleNextvbtn}
+          src={arrow}
+          alt="movenextbtn"
+        />
+
+        {/* Swiper Slider */}
+        <Swiper
+          slidesPerView={1}
+          speed={2000}
+          loop={true}
+          navigation={{
+            nextEl: ".cards-custom-next10",
+            prevEl: ".cards-custom-prev10",
+          }}
+          modules={[Keyboard, Navigation, Scrollbar, Autoplay]}
+          className="mySwiper"
+        >
+          {media?.map((item, index) => (
+            <SwiperSlide key={index}>
+              <MediaSlide
+                src={item.src}
+                type={item.type}
+                setImageLoaded={index === 0 ? setImageLoaded : undefined}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* Gradient Overlay */}
+        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(255,255,255,0.1)] to-[rgba(0,0,0,0.5)] pagination-gradient z-10 pointer-events-none"></div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkSlider;
