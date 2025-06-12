7a2d4973a41b43ae9641f1ca9e17a78f5de7c639
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2025-06-12 05:09:16 +0500

    menu updated

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -17,10 +17,6 @@ import "./Navbar.css";
 import { cn } from "@/libs/utils/twMerge";
 
 const services = [
-  { name: "ADU", slug: "adus" },
-  { name: "Architectural Services", slug: "architectural-services" },
-  { name: "Bathroom Remodeling", slug: "bathroom-remodeling" },
-  { name: "Decks & Patios", slug: "decks" },
   { name: "Exterior Remodeling", slug: "exterior-remodeling" },
   { name: "Kitchen Remodeling", slug: "kitchen-remodeling" },
   { name: "Bathroom Remodeling", slug: "bathroom-remodeling" },
