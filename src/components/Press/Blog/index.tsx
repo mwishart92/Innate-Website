298d15f67a40dca00d1d0bf1ee4b1a1e4ff1eeec
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2025-07-03 22:27:16 +0500

    removed two blogs

--- src/components/Press/Blog/index.tsx
+++ src/components/Press/Blog/index.tsx
@@ -19,7 +19,7 @@ const Blog = () => {
   return (
     <div className=" pt-16">
       <div className="w-full max-w-full  flex flex-wrap justify-center gap-[24px] mb-[44px] mob:mb-[24px] px-16 md:px-20 mob:px-0">
-        <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
+        <div className="flex-grow max-w-[calc(25%-18px)] tab:max-w-[calc(50%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
           <Image
             src={blog1}
             alt=""
@@ -60,7 +60,7 @@ const Blog = () => {
           </div>
         </div>
 
-        <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px] flex flex-col">
+        <div className="flex-grow max-w-[calc(25%-18px)] tab:max-w-[calc(50%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px] flex flex-col">
           <Image
             src={blog2}
             alt=""
@@ -97,7 +97,7 @@ const Blog = () => {
           </div>
         </div>
 
-        <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
+        <div className="flex-grow max-w-[calc(25%-18px)] tab:max-w-[calc(50%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
           <Image
             src={blog3}
             alt=""
@@ -133,79 +133,7 @@ const Blog = () => {
             </Link>
           </div>
         </div>
-        <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
-          <Image
-            src={blog4}
-            alt=""
-            height={192}
-            className="rounded-t-xl w-[100%]"
-          />
-          <div className="flex flex-col justify-between flex-grow p-4">
-            <Link href={`#`} className="">
-              <div>
-                <Text className="text-white font-light">Blog</Text>
-                <Text className="text-white text-[20px] leading-[28px] my-2 font-normal">
-                  New construction CRM app JobSync available on apple app store
-                </Text>
-                <Text className="text-gray-400  text-[16px] font-normal">
-                  In the fast-paced world of construction management, efficiency
-                  and precision are paramount. That's why we're excited to
-                  introduce JobSync, our revolutionary proprietary software that
-                  seamlessly
-                </Text>
-              </div>
-            </Link>
-            <Link href={`#`} className="text-white flex items-center mt-5">
-              LEARN MORE{" "}
-              <Image
-                src={arrow}
-                alt=""
-                width={44}
-                height={28}
-                className="-ms-3"
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-      {/* row2 */}
-      <div className="w-full max-w-full  flex flex-wrap justify-start mob:justify-center  gap-[24px] mb-[24px] mx-auto px-16 md:px-20 mob:px-0">
-        <div className="flex-grow max-w-[calc(50%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
-          <Image
-            src={blogb}
-            alt=""
-            height={294}
-            className="rounded-t-xl w-[100%]"
-          />
-          <div className="flex flex-col justify-between flex-grow p-4">
-            <Link href={`#`} className="">
-              <div>
-                <Text className="text-white font-light">Blog</Text>
-                <Text className="text-white text-[20px] leading-[28px] my-2 font-normal">
-                  The Best CRM and Construction Project Management Software
-                </Text>
-                <Text className="text-gray-400  text-[16px] font-normal">
-                  In the intricate world of construction, effective management
-                  of both customer relationships and project timelines is
-                  paramount for success. Construction companies operate within a
-                  dynamic
-                </Text>
-              </div>
-            </Link>
-            <Link href={`#`} className="text-white flex items-center mt-5">
-              LEARN MORE{" "}
-              <Image
-                src={arrow}
-                alt=""
-                width={44}
-                height={28}
-                className="-ms-3"
-              />
-            </Link>
-          </div>
-        </div>
-
-        <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
+        <div className="flex-grow max-w-[calc(25%-18px)] tab:max-w-[calc(50%-18px)] mob:max-w-[90%] border border-[#FFFFFF99] rounded-[10px]  flex flex-col">
           <Image
             src={blog5}
             alt=""
@@ -244,6 +172,9 @@ const Blog = () => {
 
           {/* </div> */}
         </div>
+      </div>
+      {/* row2 */}
+      <div className="w-full max-w-full  flex flex-wrap justify-start mob:justify-center  gap-[24px] mb-[24px] mx-auto px-16 md:px-20 mob:px-0">
         {/* <div className="flex-grow max-w-[calc(25%-18px)] mob:max-w-[90%] border border-white rounded-[10px]">
           <Image src={blog2} alt="" height={192} className="rounded-t-xl w-[100%]" />
           <div className="p-4">
