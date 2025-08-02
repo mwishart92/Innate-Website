cdcb3d63dec213cca50251957dbfab3ffc4c7144
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2025-08-02 12:08:55 +0500

    google tagmanager plugin added and custom code removed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,7 @@ import Footer from "@/components/Footer";
 import { TabProvider } from "@/context/TabContsxt";
 import MyProvider from "@/components/MyProvider";
 
-import Script from "next/script"; // Import Script from next/script
-import GTMPageView from "@/components/GTMPageView";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const monts = Montserrat({ subsets: ["latin"], variable: "--font-monts" });
@@ -42,40 +41,17 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logo-innate.png" />
-        {/* Google Tag Manager Script */}
-        <Script
-          id="google-tag-manager"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KNSHDN66');
-            `,
-          }}
-        />
       </head>
       <body
         className={`gradient ${inter.variable} ${monts.variable} ${rajdhani.variable} ${inknut.variable} ${Bodoni.variable} bg-[#000]`}
       >
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-KNSHDN66"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
         <MyProvider>
           <TabProvider>
-            <GTMPageView />
             {children}
             <Footer />
           </TabProvider>
         </MyProvider>
+        <GoogleTagManager gtmId="GTM-KNSHDN66" />
       </body>
     </html>
   );
