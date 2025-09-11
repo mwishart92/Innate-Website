60a77954251303126f0989001cc3ad82164fef82
Rokket Web Development <60049291+rokket-web@users.noreply.github.com>
2025-09-10 17:06:09 -0700

    Update index.tsx

--- src/components/Services/Faq/index.tsx
+++ src/components/Services/Faq/index.tsx
@@ -104,7 +104,10 @@ const Faq = ({
           >
             {title}
           </Text>
-          <Text className="text-[#D9D9D9] text-center mt-3">{description}</Text>
+          <Text 
+            className="text-[#D9D9D9] text-center mt-3"
+            dangerouslySetInnerHTML={{ __html: description }}
+          />
           <div className="mt-12">
             {faqItems.map((item, index) => (
               <AccordionItem
