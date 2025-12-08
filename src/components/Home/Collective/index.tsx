edbf6c9a7432ad3d7958aa2807bd48cc57398d96
Rokket Web Development <60049291+rokket-web@users.noreply.github.com>
2025-12-08 14:55:11 -0600

    Update index.tsx

--- src/components/Home/Collective/index.tsx
+++ src/components/Home/Collective/index.tsx
@@ -14,10 +14,13 @@ const Collective = () => {
         <div className="w-[80%] mob:w-full flex flex-wrap mob:flex-col items-center justify-around">
           <div className="flex flex-col mob:px-0 w-full ipad-service pl-24 mob:pl-0">
             <Text className="text-[24px] font-medium leading-[31.2px] mb-4">
-              We're a collective of architects, engineers and craftsmen who take on complex residential projects from start to finish - design, permits, and construction all under one roof.
+            We’re a design-build team of designers, engineers, and craftsmen who take on complex residential projects from start to finish — design, permitting coordination, and construction all under one roof.
             </Text>
             <Text className="mob:text-[14px] font-normal">
-              Innate is an architecture, engineering and construction firm capable of handling the entire project from permit to build. Our approach focuses on quality design and construction that will stand the test of time. To accomplish this, we handpick consultants, craftsman and subcontractors who share our values. Detailed, beautiful and durable work is often expensive, but this cost guarantees the lasting satisfaction of a quality build.
+            Innate is a design, engineering and construction firm capable of handling the entire project from permit to build. Our approach focuses on quality design and construction that will stand the test of time. To accomplish this, we handpick consultants, licensed architects, craftsmen and subcontractors who share our values. Detailed, beautiful and durable work is often expensive, but this cost guarantees the lasting satisfaction of a quality build.
+            </Text>
+            <Text className="mob:text-[14px] font-normal">
+            Innate is a boutique design-build firm that partners with licensed architects and engineers to support the permitting process, allowing us to guide your project from feasibility to final build. Our approach focuses on quality design and construction that will stand the test of time. To accomplish this, we handpick consultants, licensed architects, craftsmen and subcontractors who share our values. Detailed, beautiful and durable work is often expensive, but this cost guarantees the lasting satisfaction of a quality build.
             </Text>
             <Button
               className="max-w-[137px] h-[50px] text-[16px] px-[24px] py-[14px] font-normal mt-7 mob:max-w-[126px] mob:h-[48px] mob:text-[14px] mob:mt-10"
