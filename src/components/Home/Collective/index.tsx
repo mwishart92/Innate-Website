fc7db3128996d26f853cdfbd403b54046c343393
Rokket Web Development <60049291+rokket-web@users.noreply.github.com>
2026-01-22 10:49:45 -0800

    Update index.tsx

--- src/components/Home/Collective/index.tsx
+++ src/components/Home/Collective/index.tsx
@@ -14,8 +14,8 @@ const Collective = () => {
         <div className="w-[80%] mob:w-full flex flex-wrap mob:flex-col items-center justify-around">
           <div className="flex flex-row mob:flex-col mob:px-0 w-full ipad-service pl-24 mob:pl-0 gap-8 mob:gap-0">
             <div className="flex-1 mob:w-full">
-              <Text className="text-[24px] font-medium leading-[31.2px] mb-4">
-                We’re a design-build team of designers, engineers, and craftsmen who take on complex residential projects from start to finish — design, permitting coordination, and construction all under one roof.
+              <Text className="text-[36px] font-regular leading-[31.2px] mb-4">
+                We’re a team of designers, engineers, and craftsmen who take on complex residential projects from start to finish — design, permitting, and construction all under one roof.
               </Text>
             </div>
             <div className="flex-1 mob:w-full flex flex-col">
