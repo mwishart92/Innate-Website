891a1f298c7e289587d83371c467e2182cb69ddf
Hasnain Shafqat <hasnainshafqatmlt@gmail.com>
2026-01-23 15:40:04 +0500

    remodeling page added with new UI

---
+++ src/app/remodeling-price-guide/page.tsx
@@ -0,0 +1,62 @@
+import Navbar from "@/components/Navbar";
+import Text from "@/components/ui/Text";
+import HeroPress from "@/components/remodeling/HeroPress";
+import Form from "@/components/remodeling/Form";
+import ManualPageTracker from "@/components/ManualPageTracker";
+import React from "react";
+import type { Metadata } from "next";
+import remodelingPhoto from "@/public/images/remodeling/remodeling-price-guide.webp";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: "Press & Media | Industry Updates and Tips | Innate NW",
+  description:
+    "Innate NW's press section features news, media coverage, and insights into their innovative design and construction projects, highlighting the industry impact.",
+  alternates: {
+    canonical: "https://innate-nw.com/press",
+  },
+  openGraph: {
+    title: "Press & Media | Industry Updates and Tips | Innate NW",
+    description:
+      "Innate NW's press section features news, media coverage, and insights into their innovative design and construction projects, highlighting the industry impact.",
+    url: "https://innate-nw.com/press",
+    siteName: "Innate NW",
+    images: [
+      {
+        url: "/images/innate-nw.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Innate NW - Press & Media",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Press & Media | Industry Updates and Tips | Innate NW",
+    description:
+      "Innate NW's press section features news, media coverage, and insights into their innovative design and construction projects, highlighting the industry impact.",
+  },
+};
+
+const RemodelingPriceGuide = () => {
+  return (
+    <>
+      <ManualPageTracker pageName="remodeling-price-guide_page" />
+      <Navbar />
+      <HeroPress />
+      <div className="gradient">
+        <Image
+          src={remodelingPhoto}
+          alt=""
+          fill
+          className="w-full h-auto !relative"
+        />
+        <div>
+          <Form />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default RemodelingPriceGuide;
