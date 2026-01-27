536508bbbd623d4d918e1b64393856c2c9adbd98
Rokket Web Development <60049291+rokket-web@users.noreply.github.com>
2026-01-27 10:13:51 -0800

    Update index.tsx

--- src/components/Home/Hero/Slider/index.tsx
+++ src/components/Home/Hero/Slider/index.tsx
@@ -134,7 +134,7 @@ function HomeSlider() {
             </SwiperSlide>
           ))}
         </Swiper>
-        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(255,255,255,0.1)] to-[rgba(0,0,0,1)]  pagination-gradient bg-black/40 z-10 pointer-events-none"></div>
+        <div className="absolute inset-0 bg-gradient-to-b from-[rgba(255,255,255,0.1)] to-[rgba(0,0,0,1)]  pagination-gradient z-10 pointer-events-none" style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}></div>
       </div>
     </div>
   );
